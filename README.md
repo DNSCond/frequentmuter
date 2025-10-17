1d7b1f68136bcbb950e918017a1c630687144c0b
qin2007 <qin@ractoc.com>
2025-10-17 13:37:01 +0200

    muteTimeCustom

--- README.md
+++ README.md
@@ -15,3 +15,18 @@ like that. which is why i built this thing. it mutes anyone who (sends `X` messa
 oh yeah, you need to specify `Y` in seconds, but a quick google search will help.
 
 [source code at: https://github.com/DNSCond/frequentmuter.git](https://github.com/DNSCond/frequentmuter.git)
+
+to reset the tracker either wait Y seconds or speak to them in modmail. just unmuting them will not work.
+
+note that it may not immdietly work if the user is fast
+
+## changelog
+
+### 0.0.6 (at 2025-10-17): Customizzed mutes
+
+- you can now set a custom mute duration, which i mean the bot will try to unmute the user when it esaplses.
+- if custom mute duration is set a line like `You will be automatically unmuted at about Fri Oct 17 2025 11:27 (Coordinated Universal Time)`
+  will be added to inform the user. futher versions may disable it
+- other things.
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Devvit, TriggerContext } from "@devvit/public-api";
 import { ModMail } from "@devvit/protos";
+import { EXMAScript, jsonEncode } from "anthelpers";
 
 Devvit.configure({ redditAPI: true, redis: true });
 const OneDayInSeconds = 86400, OneHourInSeconds = 3600;
@@ -22,9 +23,9 @@ Devvit.addSettings([
         type: 'number',
         name: 'messages',
         label: 'the number of messages to allow',
-        helpText: 'must be betwen 2 and 20 (define X here)',
+        helpText: 'must be betwen 1 and 20 (define X here)',
         defaultValue: 4, //required: true,
-        onValidate: validateRangeInt('X', 2, 20),
+        onValidate: validateRangeInt('X', 1, 20),
       },
       {
         type: 'select',
@@ -33,15 +34,24 @@ Devvit.addSettings([
         defaultValue: ['72'],
         //required: true,
         options: [
-          { label: '3 days', value: '72' },
-          { label: '7 days', value: '168' },
-          { label: '28 days', value: '672' },
+          { label: '3 days (259 200s)', value: '72' },
+          { label: '7 days (604 800s)', value: '168' },
+          { label: '28 days (2 419 200s)', value: '672' },
         ],
+      },
+      {
+        type: 'number',
+        name: 'muteTimeCustom',
+        label: 'have the bot automatically unmute a user',
+        helpText: `should not be greater than Z, must be between 17 and ${OneDayInSeconds * 27} inclusive, or 0 to not automatically unmute`,
+        defaultValue: 0, //required: true,
+        onValidate: validateRangeInt('X', 17, OneDayInSeconds * 27, true),
       },
       {
         type: 'paragraph',
         name: 'messageUponMute',
         label: 'a message to send when the author gets muted. not including the default',
+        helpText: 'supports modmail markdown',
         defaultValue,
       },
     ],
@@ -60,7 +70,8 @@ Devvit.addTrigger({
 
     const conversationResponse = await context.reddit.modMail.getConversation({
       conversationId: event.conversationId,
-    }), { conversation } = conversationResponse; // , date = new Date(event.createdAt ?? new Date);
+    }), { conversation } = conversationResponse;
+    // , date = new Date(event.createdAt ?? new Date);
     if (!conversation) return;
     if (!event.messageAuthor) return;
 
@@ -73,17 +84,15 @@ Devvit.addTrigger({
     const isMod = Boolean(Object(currentMessage.author).isMod),
       isAdmin = Boolean(Object(currentMessage.author).isAdmin);
     const conversationId = event.conversationId.split(/_/)[1];
-    const userId = event.messageAuthor.id;
-    const key = `userId-${userId}`;
+    const userId = event.messageAuthor.id, username = event.messageAuthor.name;
+    const { key, muteDurationRedisKey } = getKeyFromUserId(userId);
     const { redis } = context;
     if (isMod || isAdmin) {
       if (isMod) {
         // conversation.participant?.id = about 153573674696316
-
         const userName = conversation.participant?.name;
-        if (userName) {
-          const key = `userId-${(await context.reddit.getUserByUsername(userName))?.id}`;
-          await redis.del(key);
+        if (userName && username !== 'frequentmuter') {
+          await removeRedisKey((await context.reddit.getUserByUsername(userName))?.id, context);
         }
       }
       return;
@@ -92,26 +101,68 @@ Devvit.addTrigger({
     if (!timeframe) return;
     const messagesSent = await redis.incrBy(key, 1);
     await redis.expire(key, +timeframe);
-    const messagesRequireMent = await context.settings.get('messages'),
-      muteTime = (await context.settings.get('muteTime'))!;
-    //console.log(printLn({ muteTime, messagesRequireMent, messagesSent, }));
-    if (messagesRequireMent && +muteTime) {
-      if (messagesSent > +messagesRequireMent) {
-        const body = String((await context.settings.get('messageUponMute')) || defaultValue);
+    const messagesRequireMent = await context.settings.get<number>('messages'),
+      muteTimeCustom = +(await context.settings.get<number>('muteTimeCustom') as number),
+      muteTime = +(await context.settings.get('muteTime'))!, hasCustom = !Number.isNaN(muteTimeCustom);
+    if (messagesRequireMent && muteTime) {
+      if (messagesSent > messagesRequireMent) {
+        const now = Date.now(), runAt = ResolveSecondsAfter(muteTimeCustom, now);
+        let body = String((await context.settings.get('messageUponMute')) || defaultValue);
+        if (hasCustom) {
+          body += `\n\nYou will be automatically unmuted at about ${sliceOut(String(runAt), 21, 33, true)}`;
+        }
         await context.reddit.modMail.reply({
           body, conversationId,
           isAuthorHidden: true,
         });
         await context.reddit.modMail.muteConversation({
           conversationId, // @ts-expect-error
-          numHours: +muteTime,
+          numHours: muteTime,
         });
+        if (hasCustom) {
+          const jobId = await context.scheduler.runJob({
+            name: 'unmuteTimeCustom', runAt,
+            data: { userId, conversationId, },
+          });
+          const expiration = runAt;// being set here
+          await context.redis.set(muteDurationRedisKey, jsonEncode({ expiration, jobId }), { expiration });
+        }
       }
     }
   },
 });
 
+Devvit.addSchedulerJob({
+  name: 'unmuteTimeCustom', async onRun(event, context) {
+    if (event.data) {
+      const { userId, conversationId }: { userId: string, conversationId: string } = event.data as any;
+      const conversation = await context.reddit.modMail.getConversation({ conversationId });
+      if (conversation?.conversation) {
+        await context.reddit.modMail.unmuteConversation(conversationId);
+        console.log(`userId=(${userId}) is unmuted`);
+        await removeRedisKey(userId, context);
+      }
+    }
+  },
+});
 
+function getKeyFromUserId(userId?: string) {
+  const key = `userId-${userId}`, muteDurationRedisKey = key + '-muteDuration';
+  return { key, muteDurationRedisKey }; // = getKeyFromUserId(userId);
+}
+
+async function removeRedisKey(userId: string | undefined, { redis, scheduler }: TriggerContext) {
+  const { key, muteDurationRedisKey } = getKeyFromUserId(userId);
+  console.log('removed', printLn({ userId }), 'from redis');
+
+  const jobIdJson = await redis.get(muteDurationRedisKey);
+  await redis.del(key); await redis.del(muteDurationRedisKey);
+  if (jobIdJson) {
+    const jobId = JSON.parse(jobIdJson)['jobId'];
+    await scheduler.cancelJob(jobId);
+  }
+  return { key, muteDurationRedisKey };
+}
 
 const usernameEvalForm = Devvit.createForm(
   {
@@ -126,11 +177,8 @@ const usernameEvalForm = Devvit.createForm(
     ],
     title: 'Evaluate User',
     acceptLabel: 'Submit',
-  },
-  async function (event, context) {
+  }, async function (event, context) {
     const currentUsername = await context.reddit.getCurrentUsername();
-
-
     if (currentUsername === undefined) return;
     const username: string = event.values.username.trim().replace(/^u\//, '') ?? '[undefined]';
     if (/^[a-zA-Z0-9\-_]+$/.test(username)) {
@@ -139,12 +187,20 @@ const usernameEvalForm = Devvit.createForm(
         context.ui.showToast(`username does not exist or devvit couldnt find it`);
         return;
       }
-      const messagesSent = await context.redis.get(`userId-${id}`);
-      context.ui.showToast(`they send ${messagesSent} messages in the timeframe. speak to them as moderator to reset the timeframe`);
+      const { key, muteDurationRedisKey } = getKeyFromUserId(id);
+      const messagesSent = await context.redis.get(key);
+      const muteDuration = await context.redis.get(muteDurationRedisKey);
+
+      let text = `they send ${messagesSent ?? 0} messages in the timeframe. speak to them as moderator to reset the timeframe.`;
+      if (muteDuration) {
+        const d = new Date(JSON.parse(muteDuration)['expiration']);
+        text = `they are currently muted and will be unmuted by the bot at ${d}`;
+      }
+      context.ui.showToast({ text });
     } else if (username === '[undefined]') {
       context.ui.showToast({ text: `there was no username given` });
     } else {
-      context.ui.showToast({ text: `that username is syntactically invalid` });
+      context.ui.showToast(`that username is syntactically invalid`);
     }
   }
 );
@@ -160,11 +216,11 @@ Devvit.addMenuItem({
 });
 
 export default Devvit; // validateRangeInt('', minInclusive, maxInclusives)
-function validateRangeInt(variable: string, minInclusive: number, maxInclusive: number) {
+function validateRangeInt(variable: string, minInclusive: number, maxInclusive: number, allow0: boolean = false) {
   return function ({ value }) {
     try {
       // @ts-expect-error
-      const b = BigInt(value);
+      const b = BigInt(value); if (allow0) if (b === 0n) return undefined;
       if (b < minInclusive) throw new RangeError(`${variable} must be greater than ${minInclusive}, received ${b}`);
       if (b > maxInclusive) throw new RangeError(`${variable} must be less than ${maxInclusive}, received ${b}`);
     } catch (err) {
@@ -214,4 +270,57 @@ function printLn(varaibles: any) {
 
 function ResolveSecondsAfter(s: number = 0, now?: Date | string | number): Date {
   return new Date((new Date(now ?? Date.now())).setMilliseconds(0) + (+s) * 1000);
+}
+
+function sliceOut(string: string, start?: number, end?: number, strict: boolean = false) {
+  if (string === undefined || string === null) throw new TypeError('sliceOut RequireObjectCoercible is false');
+  string = `${string}`;
+  const len = string.length;
+  const intStart = EXMAScript.toIntegerOrInfinity(start);
+  let from;
+  if (strict) {
+    from = intStart;
+  } else {
+    if (intStart === -Infinity) {
+      from = 0;
+    } else if (intStart < 0) {
+      from = Math.max(len + intStart, 0);
+    } else {
+      from = Math.min(intStart, len);
+    }
+  }
+  let intEnd;
+  if (end === undefined) {
+    intEnd = len;
+  } else {
+    intEnd = EXMAScript.toIntegerOrInfinity(end);
+  }
+  let to;
+  if (strict) {
+    to = intEnd;
+  } else {
+    if (intEnd === -Infinity) {
+      to = 0;
+    } else if (intEnd < 0) {
+      to = Math.max(len + intEnd, 0);
+    } else {
+      to = Math.min(intEnd, len);
+    }
+  }
+  if (from >= to) {
+    if (strict) {
+      throw new RangeError(`the normalized value of start (${from}) is greater than the normalized value of end (${to}) in sliceOut_String\'s strict mode`);
+    } else {
+      return "";
+    }
+  }
+  if (strict) {
+    if (from < 0) {
+      throw new RangeError(`start is less than 0 in sliceOut_String\'s strict mode (got ${from})`);
+    }
+    if (to > len) {
+      throw new RangeError(`end is greater than ${len} in sliceOut_String\'s strict mode (got ${to})`);
+    }
+  }
+  return (string.slice(0, from) + string.slice(to));
 }
